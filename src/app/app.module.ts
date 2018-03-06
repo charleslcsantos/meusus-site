2ffb8f6e6e2467279262be9e2f8cf3f06d94df01
Charles Lennon <charleslennon@institutoeducar.com.br>
2018-03-06 18:38:57 -0300

    adicionando click

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -21,6 +22,7 @@ import { TransferHttpCacheModule } from '@nguniversal/common';
       { path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
     ]),
     TransferHttpCacheModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/search-input/search-input.component.ts
+++ src/app/shared/search-input/search-input.component.ts
@@ -7,10 +7,20 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class SearchInputComponent implements OnInit {
+  public searchTerm = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  /**
+   * search
+   */
+  public search() {
+    if (this.searchTerm) {
+      console.log(this.searchTerm);
+    }
   }
 
 }
